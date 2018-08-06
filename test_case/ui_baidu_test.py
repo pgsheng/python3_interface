3bcd71c895c190858c53faaf3adec65109fc3fc6
pgsheng <pgsheng09@163.com>
2018-08-06 20:53:11 +0800

    优化

--- test_case/ui_baidu_test.py
+++ test_case/ui_baidu_test.py
@@ -1,9 +1,15 @@
 # coding=utf-8
 import unittest
+
+from ddt import ddt, data
+
 from pageobjects.baidu_homepage import HomePage
+from public import base
 from public.log import Log
 
+testdata = base.get_excel_data('test.xlsx', 'test2')
 
+@ddt
 class BaiduSearch(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -17,26 +23,23 @@ class BaiduSearch(unittest.TestCase):
     #     self.homepage.sleep(2)
     #     self.homepage.back()
 
-    def test_baidu_search(self):
-        self.home_page.sleep(2)
-        self.home_page.search_input('selenium')  # 调用页面对象中的方法
+    @data(*testdata)
+    def test_1baidu_search(self,data):
+        self.home_page.sleep(1)
+        self.home_page.search_input(data.get("name"))  # 调用页面对象中的方法
         self.home_page.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
-        self.home_page.sleep(2)
-        self.home_page.take_screenshot('selenium')  # 调用基类截图方法
-        self.home_page.sleep(2)
+        self.home_page.sleep(1)
+        self.home_page.take_screenshot(data.get("name"))  # 调用基类截图方法
         self.home_page.back()
-        try:
-            assert 'selenium' in self.home_page.get_title()  # 调用页面对象继承基类中的获取页面标题方法
-            self.log.info('Test Pass.')
-        except Exception as e:
-            self.log.exception(e)
+        self.assertIn(data.get("name"), self.home_page.get_title(), msg='和预期不一样')
 
-    def test_baidu_search2(self):
-        self.home_page.sleep(2)
+    def test_2baidu_search(self):
+        self.home_page.sleep(1)
         self.home_page.search_input('python')  # 调用页面对象中的方法
         self.home_page.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
-        self.home_page.sleep(2)
+        self.home_page.sleep(1)
         self.home_page.take_screenshot('python')  # 调用基类截图方法
+        self.assertIn("python", self.home_page.get_title(), msg='和预期不一样')
 
     @classmethod
     def tearDownClass(self):
