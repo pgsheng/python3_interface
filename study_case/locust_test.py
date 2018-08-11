00437b764ec34f50933b31958fdd28145c0a6548
pgsheng <pgsheng09@163.com>
2018-08-11 22:41:41 +0800

    优化

--- study_case/locust_test.py
+++ study_case/locust_test.py
@@ -6,44 +6,62 @@
 '''
 import gevent
 from gevent import monkey
+
 gevent.monkey.patch_all()
 import requests
 from locust import HttpLocust, TaskSet, task
 
-# cd study_case
-# locust -f locust_test.py --host=https://httpbin.org
-# localhot:8089/
+"""
+   继承TaskSet类，为用户行为
+   """
+
+"""
+@task() 装饰该方法为一个任务。1表示一个Locust实例被挑选执行的权重，数值越大，
+执行频率越高
+"""
+
+
 class UserBehavior(TaskSet):
 
-	@task(1)
-	def test_get(self):
-		params = {'show_envs': '1'}
-		with self.client.get("/get",params=params, catch_response=True) as response:
-			if response.status_code == 200:
-				print(response.json())
-				print("success---")
-			else:
-				print("fail----")
+    @task(1)
+    def test_get(self):
+        params = {'show_envs': '1'}
+        with self.client.get("/get", params=params, catch_response=True) as response:
+            if response.status_code == 200:
+                print(response.json())
+                print("success---")
+            else:
+                print("fail----")
 
-	@task(1)
-	def test_post(self):
-		json = {
-			"info": {"code": 1, "sex": "男", "id": 1900, "name": "乔巴"},
-			"code": 1,
-			"name": "乔巴", "sex": "女",
-			"id": 1990
-		}
-		r = self.client.post("/post",data=json)
-		if r.status_code == 200:
-			print(r.json())
-			print("success---")
-		else:
-			print("fail----")
+    @task(1)
+    def test_post(self):
+        json = {
+            "info": {"code": 1, "sex": "男", "id": 1900, "name": "乔巴"},
+            "code": 1,
+            "name": "乔巴", "sex": "女",
+            "id": 1990
+        }
+        r = self.client.post("/post", data=json)
+        if r.status_code == 200:
+            print(r.json())
+            print("success---")
+        else:
+            print("fail----")
 
 
 class WebsiteUser(HttpLocust):
-	task_set = UserBehavior
-	min_wait = 3000
-	max_wait = 6000
+    """
+     设置性能测试
+     """
+    task_set = UserBehavior
+    min_wait = 3000
+    max_wait = 6000
 
 
+"""
+# cd study_case
+
+1、切换到性能测试脚本所在的目录，启动性能测试,控制台执行命令：
+ locust -f test.py --host=https://httpbin.org
+2、浏览器访问http://localhost:8089/
+"""
