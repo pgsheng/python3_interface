46bcc24a4cee9e6de2841380481b0d5b7fce80b2
pgsheng <pgsheng09@163.com>
2018-08-12 13:07:06 +0800

    优化

--- study_case/locust_study.py
+++ study_case/locust_study.py
@@ -8,20 +8,13 @@ import gevent
 from gevent import monkey
 
 gevent.monkey.patch_all()
-import requests
 from locust import HttpLocust, TaskSet, task
-
-"""
-   继承TaskSet类，为用户行为
-   """
-
-"""
-@task() 装饰该方法为一个任务。1表示一个Locust实例被挑选执行的权重，数值越大，
-执行频率越高
-"""
 
 
 class UserBehavior(TaskSet):
+    """
+       继承TaskSet类，为用户行为
+    """
 
     @task(1)
     def test_get(self):
@@ -29,9 +22,14 @@ class UserBehavior(TaskSet):
         with self.client.get("/get", params=params, catch_response=True) as response:
             if response.status_code == 200:
                 print(response.json())
-                print("success---")
+                response.success()
             else:
                 print("fail----")
+
+    """
+    @task() 装饰该方法为一个任务。1表示一个Locust实例被挑选执行的权重，数值越大，
+    执行频率越高
+    """
 
     @task(1)
     def test_post(self):
@@ -41,18 +39,18 @@ class UserBehavior(TaskSet):
             "name": "乔巴", "sex": "女",
             "id": 1990
         }
-        r = self.client.post("/post", data=json)
+        r = self.client.post("/post", data=json, catch_response=True)
         if r.status_code == 200:
             print(r.json())
-            print("success---")
+            r.success()
         else:
-            print("fail----")
+            r.failure("fail----")  # 失败断言
 
 
 class WebsiteUser(HttpLocust):
     """
      设置性能测试
-     """
+    """
     task_set = UserBehavior
     min_wait = 3000
     max_wait = 6000
@@ -62,6 +60,6 @@ class WebsiteUser(HttpLocust):
 # cd study_case
   
 1、切换到性能测试脚本所在的目录，启动性能测试,控制台执行命令：
- locust -f test.py --host=https://httpbin.org
+ locust -f locust_study.py --host=https://httpbin.org
 2、浏览器访问http://localhost:8089/
 """
