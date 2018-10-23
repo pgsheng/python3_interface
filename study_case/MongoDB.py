cde9a78e74def4cebb5f2aed29c75b6b45dfd0d7
pgsheng <pgsheng09@163.com>
2018-10-23 17:52:00 +0800

    优化

---
+++ study_case/MongoDB.py
@@ -0,0 +1,124 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+ @Author  : pgsheng
+ @Time    : 2018/10/23 14:31
+"""
+import pymongo
+
+from public.log import Log
+
+
+class MongoDB(object):
+    def __init__(self):
+        self.log = Log().get_logger()
+        """
+        1、连接MongoDB
+            连接MongoDB我们需要使用PyMongo库里面的MongoClient，一般来说传入MongoDB的IP及端口即可，第一个参数为地址host，
+            第二个参数为端口port，端口如果不传默认是27017。
+        """
+        self.client = pymongo.MongoClient(host='localhost', port=27017)
+        # client = pymongo.MongoClient('mongodb://localhost:27017/')  # 可以达到同样的连接效果。
+        """
+                2、指定数据库
+                    MongoDB分为一个个数据库，需要指定要操作哪个数据库，在这里我以test数据库为例进行说明
+                    注意: 在 MongoDB 中，数据库只有在内容插入后才会创建! 就是说，数据库创建后要创建集合(数据表)并插入一个文档(记录)，数据库才会真正创建。
+                        集合同理。
+                """
+        self.db = self.client.testdb
+        # self.db = self.client['testdb']  #　两种方式是等价的。
+
+        """
+        2.2、读取 MongoDB 中的所有数据库，并判断指定的数据库是否存在
+        """
+        dblist = self.client.list_database_names()
+        if "testdb" in dblist:
+            self.log.info("数据库已存在！")
+        else:
+            self.log.info("数据库不存在！")
+
+        """
+        3、指定集合
+            每个数据库又包含了许多集合Collection，也就类似与关系型数据库中的表，下一步需要指定要操作的集合，
+        在这里我们指定一个集合名称为students，学生集合。还是和指定数据库类似，指定集合也有两种方式。
+        """
+        self.collection = self.db.students
+        # self.collection = self.db['students']
+
+        collist = self.db.list_collection_names()
+        if "students" in collist:  # 判断 sites 集合是否存在
+            self.log.info("集合已存在！")
+        else:
+            self.log.info("集合不存在！")
+
+    def db_insert(self):
+        """
+        4、插入数据,
+            对于students这个Collection，我们新建一条学生数据，以字典的形式表示，
+            直接调用collection的insert()方法即可插入数据
+        """
+        student = {"name": "Google", "alexa": "1", "url": "https://www.google.com"}
+        result = self.collection.insert_one(student)
+        # 在MongoDB中，每条数据都有一个_id属性来唯一标识，如果没有显式指明_id，MongoDB会自动产生一个ObjectId类型的_id属性。
+        # insert_one()方法会在执行后返回的_id值。
+        self.log.info(result.inserted_id)
+
+        students = [
+            {"name": "Taobao", "alexa": "100", "url": "https://www.taobao.com"},
+            {"name": "QQ", "alexa": "101", "url": "https://www.qq.com"},
+            {"name": "Facebook", "alexa": "10", "url": "https://www.facebook.com"},
+        ]
+        results = self.collection.insert_many(students)  # 集合中插入多条数据
+        # 输出插入的所有文档对应的 _id 值
+        self.log.info(results.inserted_ids)
+
+        students = [
+            {"_id": 1, "name": "知乎", "alexa": "103", "url": "https://www.zhihu.com"},
+            {"_id": 2, "name": "Github", "alexa": "109", "url": "https://www.github.com"}
+        ]
+        results = self.collection.insert_many(students)  # 自己指定 id，插入
+        # 输出插入的所有文档对应的 _id 值
+        self.log.info(results.inserted_ids)
+
+    def db_find(self):
+        # results = self.collection.find_one()  # 查询集合中的第一条数据。
+        # self.log.info(results)
+        #
+        # for x in self.collection.find():  # 查询集合中的所有数据
+        #     self.log.info(x)
+
+        # find() 方法来查询指定字段的数据，将要返回的字段对应值设置为 1。
+        # 除了 _id 你不能在一个对象中同时指定 0 和 1，如果你设置了一个字段为 0，则其他都为 1，反之亦然。
+        # self.collection.find({}, {"name": 0, "alexa": 1}) 会抛异常
+        value_list = self.collection.find({}, {"_id": 0, "name": 1, "alexa": 1})
+        for x in value_list:
+            self.log.info(x)
+
+        self.log.info('*'*30)
+
+        # 指定条件查询
+        myquery = {"name": "Facebook"}
+        value_list = self.collection.find(myquery)
+        for x in value_list:
+            self.log.info(x)
+
+        self.log.info('*'*30)
+
+        # 高级查询,第一个字母 ASCII 值大于 "H" 的数据
+        myquery = { "name": { "$gt": "H" } }
+        value_list = self.collection.find(myquery)
+        for x in value_list:
+            self.log.info(x)
+
+
+if __name__ == '__main__':
+    m = MongoDB()
+    # m.db_insert()
+    m.db_find()
+
+"""
+常见异常：
+    1、pymongo.errors.ServerSelectionTimeoutError: localhost:27017: [WinError 10061] 
+    由于目标计算机积极拒绝，无法连接。 原因：未装MongoDB或者MongoDB服务没有开启
+    mongod --dbpath E:\MongoDB\data
+"""
