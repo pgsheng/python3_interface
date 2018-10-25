d2ba0bcc756d4e59a3cc4e756d5162676945335d
pgsheng <pgsheng09@163.com>
2018-10-25 15:16:08 +0800

    优化

--- study_case/MongoDB.py
+++ study_case/MongoDB.py
@@ -20,11 +20,11 @@ class MongoDB(object):
         self.client = pymongo.MongoClient(host='localhost', port=27017)
         # client = pymongo.MongoClient('mongodb://localhost:27017/')  # 可以达到同样的连接效果。
         """
-                2、指定数据库
-                    MongoDB分为一个个数据库，需要指定要操作哪个数据库，在这里我以test数据库为例进行说明
-                    注意: 在 MongoDB 中，数据库只有在内容插入后才会创建! 就是说，数据库创建后要创建集合(数据表)并插入一个文档(记录)，数据库才会真正创建。
-                        集合同理。
-                """
+        2、指定数据库
+            MongoDB分为一个个数据库，需要指定要操作哪个数据库，在这里我以test数据库为例进行说明
+            注意: 在 MongoDB 中，如果数据库不存在，数据库只有在内容插入后才会创建! 就是说，数据库创建后要创建集合(数据表)
+                并插入一个文档(记录)，数据库才会真正创建。集合创建同理。
+        """
         self.db = self.client.testdb
         # self.db = self.client['testdb']  #　两种方式是等价的。
 
@@ -60,7 +60,6 @@ class MongoDB(object):
         student = {"name": "Google", "alexa": "1", "url": "https://www.google.com"}
         result = self.collection.insert_one(student)
         # 在MongoDB中，每条数据都有一个_id属性来唯一标识，如果没有显式指明_id，MongoDB会自动产生一个ObjectId类型的_id属性。
-        # insert_one()方法会在执行后返回的_id值。
         self.log.info(result.inserted_id)
 
         students = [
@@ -113,20 +112,79 @@ class MongoDB(object):
         self.log.info('-' * 20)
 
         # 使用正则表达式查询,第一个字母为 "F" 的数据
-        myquery = {"name": {"$regex": "^F" }}
+        myquery = {"name": {"$regex": "^F"}}
         value_list = self.collection.find(myquery)
         for x in value_list:
             self.log.info(x)
+
+        self.log.info('-' * 20)
+
+        # 返回指定条数记录，设置指定条数的记录可以使用 limit() 方法，该方法只接受一个数字参数
+        value_list = self.collection.find().limit(2)
+        for x in value_list:
+            self.log.info(x)
+
+    def db_update(self):
+        value_list = self.collection.find()  # 查询集合中的所有数据
+        for x in value_list:
+            self.log.info(x)
+
+        # # 将 alexa 字段的值为XXX 的改为 XXX
+        # myquery = {"alexa": "103"}
+        # newvalues = {"$set": {"alexa": "111"}}
+        # result = self.collection.update_one(myquery, newvalues) # 只能修匹配到的第一条记录
+        # self.log.info('修改数据结果：%s' % result.modified_count)
+
+        # 将 alexa 字段的值为10000 的改为 111
+        myquery = {"alexa": "111"}
+        newvalues = {"$set": {"alexa": "112"}}
+        result = self.collection.update_many(myquery, newvalues)  # 修改所有匹配到的记录
+        self.log.info('修改数据结果：%s' % result.modified_count)
+
+        self.log.info('-' * 20)
+
+        value_list = self.collection.find()  # 查询集合中的所有数据
+        for x in value_list:
+            self.log.info(x)
+
+    def db_sort(self):
+        # value_list = self.collection.find().sort("alexa")  # 对字段 alexa 按升序排序
+        value_list = self.collection.find().sort("alexa", -1)  # 对字段 alexa 按降序排序
+        for x in value_list:
+            self.log.info(x)
+
+    def db_delete(self):
+        # myquery = {"name": "Taobao"}
+        # self.collection.delete_one(myquery)# 删除 name 字段值为 "Taobao" 的第一个匹配文档
+
+        myquery = {"name": {"$regex": "^G"}}
+        result = self.collection.delete_many(myquery)# 删除所有 name 字段中以 G 开头的文档
+        self.log.info('删除结果：%s' % result.deleted_count)
+
+        value_list = self.collection.find()
+        for x in value_list:
+            self.log.info(x)
+
+        # self.collection.drop() # 删除一个集合
+        # collist = self.db.list_collection_names()
+        # if "students" in collist:  # 判断 sites 集合是否存在
+        #     self.log.info("集合已存在！")
+        # else:
+        #     self.log.info("集合不存在！")
 
 
 if __name__ == '__main__':
     m = MongoDB()
     # m.db_insert()
-    m.db_find()
+    # m.db_find()
+    # m.db_update()
+    # m.db_sort()
+    m.db_delete()
 
 """
 常见异常：
     1、pymongo.errors.ServerSelectionTimeoutError: localhost:27017: [WinError 10061] 
     由于目标计算机积极拒绝，无法连接。 原因：未装MongoDB或者MongoDB服务没有开启
-    mongod --dbpath E:\MongoDB\data
+    2、启动服务：mongod --dbpath E:\MongoDB\data  或 net start MongoDB（管理员打开cmd）
+
 """
