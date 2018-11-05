b8c51b7c22b3dd80ebf197378d9cd7be07a4fe70
pgsheng <pgsheng09@163.com>
2018-11-05 17:47:30 +0800

    优化

---
+++ study_case/MySQLStudy.py
@@ -0,0 +1,55 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+ @Author  : pgsheng
+ @Time    : 2018/10/23 14:31
+"""
+import MySQLdb
+
+from public.log import Log
+
+
+class MySQLStudy(object):
+    def __init__(self):
+        self.log = Log().get_logger()
+        # 打开数据库连接
+        self.db = MySQLdb.connect(host='localhost',
+                                  port=3306,
+                                  user='root',
+                                  passwd='123456',
+                                  charset='utf-8')
+        self.cursor = self.db.cursor()
+
+    def db_create(self):
+        self.cursor.execute('SELECT VERSION()')
+        data = self.cursor.fetchone()
+        print('Database version:', data)
+        self.cursor.execute("CREATE DATABASE teacherdb DEFAULT CHARACTER SET utf8")
+        self.db.close()
+
+    def db_insert(self):
+        pass
+
+    def db_find(self):
+        pass
+
+    def db_update(self):
+        pass
+
+    def db_sort(self):
+        pass
+
+    def db_delete(self):
+        pass
+
+
+if __name__ == '__main__':
+    m = MySQLStudy()
+    m.db_create()
+
+"""
+123456
+启动：mysqld --console 关闭窗口即关闭
+      net start mysql 关闭窗口服务还是打开的，关闭方式 net stop mysql
+
+"""
