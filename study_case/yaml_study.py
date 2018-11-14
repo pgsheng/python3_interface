20edb5863040766caa75b0e6de07c6994ad52598
pgsheng <pgsheng09@163.com>
2018-11-14 17:46:59 +0800

    优化

--- study_case/yaml_study.py
+++ study_case/yaml_study.py
@@ -4,6 +4,7 @@
  @Author  : pgsheng
  @Time    : 2018/4/17 9:02
 """
+import csv
 import json
 import os
 import yaml
@@ -65,35 +66,53 @@ def read_json():
     with open(path, 'r', encoding='utf-8') as file:
         load_data = json.load(file) # 文件流对象
         print(load_data)
-        # print(load_data.get('date'))
 
     with open(path, 'r', encoding='utf-8') as file:
         load_all_data = json.loads(file.read()) # 字符串
         for data in load_all_data:
             print(data)
-            # print(data.get('complex'))
 
     with open(path, 'r', encoding='utf-8') as file:
         load_all_data = file.read()
         print(load_all_data) # 字符串 str
 
 
-
 def write_json():
-    # 你的yaml格式文件路径
-    path = config.project_path + r'\study_case\data\yaml_study_data.yaml'
-    # 待写入的数据
+    path = os.path.join(config.study_case_path,'data','sina7x24.json')
     data = {'complex2': {'languages': ['Ruby', 'Perl', 'Python'],
                          'websites': {'YAML': 'yaml.org', 'Ruby': 'ruby-lang.org', 'Python': 'python.org'}}}
-
     # a追加写入，w覆盖写入
-    with open(path, 'a', encoding='utf-8') as file:
+    with open(path, 'w', encoding='utf-8') as file:
         # 将python对象转换成为yaml格式文档
-        yaml.dump(data, file)
+        json.dump(data, file)
 
+def read_csv():
+    path = os.path.join(config.study_case_path,'data','teachers.csv')
+
+
+    with open(path, 'r', newline='') as file:
+        load_data = csv.reader(file) # 文件流对象
+        for data in load_data:
+            print(data)
+
+    return load_data
+
+
+def write_csv():
+    path = os.path.join(config.study_case_path,'data','teachers1.csv')
+    # a追加写入，w覆盖写入
+    with open(path, 'a', encoding='utf-8',newline='') as file:
+    # file = open(path, 'a', newline='')
+        csv_write = csv.writer(file, dialect='excel')
+        csv_items = [['郭老师', '高级讲师', '及入侵分析。']]
+        for item in csv_items:
+            csv_write.writerow(item)
 
 if __name__ == '__main__':
     # read_yaml()
 # write_yaml()
 #     read_txt()
-    read_json()
+#     read_json()
+#     write_json()
+#     read_csv()
+    write_csv()
