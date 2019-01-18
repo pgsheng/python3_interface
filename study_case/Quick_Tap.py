6c7e3952393938f2578bfd36be5d74feeb17966c
pgsheng <pgsheng09@163.com>
2019-01-18 15:35:27 +0800

    优化

--- study_case/Quick_Tap.py
+++ study_case/Quick_Tap.py
@@ -9,13 +9,11 @@ from threading import Lock
 
 mutex = Lock()
 
-
 def test(x, y):
     while True:
         # mutex.acquire()
         os.system('adb shell input tap {} {}'.format(x, y))
         # mutex.release()
-
 
 if __name__ == '__main__':
     for i in range(10):
