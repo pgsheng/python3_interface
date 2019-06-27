15933630d47eb120de162f7735d07a1be6f7e54c
pgsheng <pgsheng09@163.com>
2019-06-27 15:02:48 +0800

    优化

--- study_case/wxPython_study.py
+++ study_case/wxPython_study.py
@@ -1,44 +1,38 @@
 import wx  # 导入wxPython
 
 
-class App(wx.App):
-    def OnInit(self):  # 初始化方法
-        frame = wx.Frame(parent=None, id=-1, title='程序', pos=(200, 200), size=(300, 300))  # 创建顶级窗口
+class MyFrame(wx.Frame):
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, id, title="创建StaticText类",
+                           pos=(200, 200), size=(400, 400))
 
-        panel = wx.Panel(self)  # 创建画板
-        # 创建标题，并设置字体
-        title = wx.StaticText(panel, label='Python之禅——Tim Peters', pos=(100, 20))
+        panel = wx.Panel(self)  # 创建画板，将panel 作为父类，然后将组件放入窗体中
+
+        # 创建文本和输入框
+        self.title = wx.StaticText(panel, label="请输入用户名和密码", pos=(140, 20))
         font = wx.Font(16, wx.DEFAULT, wx.FONTSTYLE_NORMAL, wx.NORMAL)
-        title.SetFont(font)
-        # 创建文本
-        wx.StaticText(panel, label='优美胜于丑陋', pos=(50, 50))
-        wx.StaticText(panel, label='明了胜于晦涩', pos=(50, 70))
-        wx.StaticText(panel, label='简洁胜于复杂', pos=(50, 90))
-        wx.StaticText(panel, label='复杂胜于凌乱', pos=(50, 110))
-        wx.StaticText(panel, label='扁平胜于嵌套', pos=(50, 130))
-        wx.StaticText(panel, label='间隔胜于紧凑', pos=(50, 150))
-        wx.StaticText(panel, label='可读性很重要', pos=(50, 170))
-        wx.StaticText(panel, label='即便假借特例的实用性之名，也不可违背这些规则', pos=(50, 190))
-        wx.StaticText(panel, label='不要包容所有错误，除非你确定需要这样做', pos=(50, 210))
-        wx.StaticText(panel, label='当存在多种可能，不要尝试去猜测', pos=(50, 230))
-        wx.StaticText(panel, label='而是尽量找一种，最好是唯一一种明显的解决方案', pos=(50, 250))
-        wx.StaticText(panel, label='虽然这并不容易，因为你不是 Python 之父', pos=(50, 270))
-        wx.StaticText(panel, label='做也许好过不做，但不假思索就动手还不如不做', pos=(50, 290))
-        wx.StaticText(panel, label='如果你无法向人描述你的方案，那肯定不是一个好方案;反之亦然', pos=(50, 310))
-        wx.StaticText(panel, label='命名空间是一种绝妙的理念，我们应当多加利用', pos=(50, 330))
+        self.title.SetFont(font)  # 设置字体
+        self.label_user = wx.StaticText(panel, label="用户名:", pos=(50, 50))
+        self.text_user = wx.TextCtrl(panel, pos=(100, 50), size=(235, 25), style=wx.TE_LEFT,value='请输入用户名')
+        # print(self.text_user)
+        self.label_pwd = wx.StaticText(panel, pos=(50, 90), label="密   码:")
+        self.text_password = wx.TextCtrl(panel, pos=(100, 90), size=(235, 25), style=wx.TE_PASSWORD)
+        # print(self.text_password)
 
-
-        frame.Show()  # 显示窗口
-        return True  # 返回值(返回窗口，在屏幕展示)
-
-
+        # 创建“确定”和“取消”按钮, 并绑定事件
+        self.bt_confirm = wx.Button(panel, label='确定')
+        # self.bt_confirm.Bind(wx.EVT_BUTTON, self.OnclickSubmit)
+        self.bt_cancel = wx.Button(panel, label='取消')
+        # self.bt_cancel.Bind(wx.EVT_BUTTON, self.OnclickCancel)
 
 if __name__ == '__main__':
-    app = App()  # 实例化App类
-    app.MainLoop()  # 调用App类的MainLoop()主循环方法
+    app = wx.App()                      # 初始化应用
+    frame = MyFrame(parent=None, id=-1)  # 实例MyFrame类，并传递参数
+    frame.Show()                        # 显示窗口
+    app.MainLoop()                      # 调用主循环方法
 
 '''
-wx.Frame(parent=None, id=-1, title="", pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.DfEFAULT_DIALOG_STYLE, name="frame")
+一、wx.Frame(parent=None, id=-1, title="", pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.DfEFAULT_DIALOG_STYLE, name="frame")
 
 parent：框架的父窗口。如果是顶级窗口，这个值是None
 id：关于新窗口的wxPython ID号。通常设为-1，让wxPython自动生成一个新的ID
@@ -47,4 +41,17 @@ pos：wx.Point对象，指定左上角在屏幕位置。这个默认值(-1,-1)�
 size：一个wx.Python对象，指定这个窗口的初始尺寸，这个默认值(-1,-1)将让系统决定窗口的初始尺寸
 style：指定窗口的类型的常量。可以使用或运算来组合他们。
 name：框架内在的名字。可以使用它来寻找这个窗口
+
+二、wx.TextCtrl(parent, id, value="", pos=wx.DefaultPosition, size=wx.DefaultSize,style=0,validator=wx.DefaultValidator name=wx.TextCtrlNameStr)
+style：单行wx.TextCtrl的样式，取值如下：
+        wx.TE_CENTER：控件中的文本居中
+        wx.TE_LEFT：控件中的文本左对齐
+        wx.TE_NOHIDESEL：文本始终高亮显示，仅适用于Windows
+        wx.TE_PASSWORD：不显示所键入的文本，以（*）代替显示
+        wx.TE_PROCESS_ENTER：如果使用改参数，那么当用户在控件内按下Enter键时，一个文本输入事件将被触发。否则，按键事件由该文本控件或该对话框管理
+        wx.TE_PROCESS_TAB：如果指定了这个样式，那么通常的字符事件在按下Tab键时创建（一般意味着一个制表符将被插入文本）。否则，tab由对话框来管理，通常是控件间切换
+        wx.TE_READONLY：文本控件为只读，用户不能修改其中文本
+        wx.TE_RIGHT：控件中的文本右对齐
+value：显示在该控件中的初始文本
+validator：常用于过滤数据以确保只能键入要接受的数据
 '''
---
+++ study_case/wxPython_study2.py
@@ -0,0 +1,64 @@
+import wx  # 导入wxPython
+
+
+class MyFrame(wx.Frame):
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, id, '用户登录', size=(400, 300))
+        # 创建面板
+        panel = wx.Panel(self)
+        # 创建文本，左对齐
+        self.title = wx.StaticText(panel, label="请输入用户名和密码")
+        self.label_user = wx.StaticText(panel, label="用户名:")
+        self.text_user = wx.TextCtrl(panel, style=wx.TE_LEFT)
+        self.label_pwd = wx.StaticText(panel, label="密   码:")
+        self.text_password = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+        # 创建“确定”和“取消”按钮, 并绑定事件
+        self.bt_confirm = wx.Button(panel, label='确定')
+        self.bt_confirm.Bind(wx.EVT_BUTTON, self.OnclickSubmit)
+        self.bt_cancel = wx.Button(panel, label='取消')
+        self.bt_cancel.Bind(wx.EVT_BUTTON, self.OnclickCancel)
+        # 添加容器，容器中控件按横向并排排列
+        horizontal_user = wx.BoxSizer(wx.HORIZONTAL)
+        horizontal_user.Add(self.label_user, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL, border=5)
+        horizontal_user.Add(self.text_user, proportion=1, flag=wx.ALL, border=5)
+
+        horizontal_pwd = wx.BoxSizer(wx.HORIZONTAL)
+        horizontal_pwd.Add(self.label_pwd, proportion=0, flag=wx.ALIGN_CENTER_VERTICAL, border=5)
+        horizontal_pwd.Add(self.text_password, proportion=1, flag=wx.ALL, border=5)
+
+        horizontal_button = wx.BoxSizer(wx.HORIZONTAL)
+        horizontal_button.Add(self.bt_confirm, proportion=0, flag=wx.ALIGN_CENTER, border=5)
+        horizontal_button.Add(self.bt_cancel, proportion=1, flag=wx.ALIGN_CENTER, border=5)
+        # 添加容器，容器中控件按纵向并排排列
+        vsizer_all = wx.BoxSizer(wx.VERTICAL)
+        vsizer_all.Add(self.title, proportion=0, flag=wx.BOTTOM | wx.TOP | wx.ALIGN_CENTER,
+                       border=15)
+        vsizer_all.Add(horizontal_user, proportion=0, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=45)
+        vsizer_all.Add(horizontal_pwd, proportion=0, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=45)
+        vsizer_all.Add(horizontal_button, proportion=0, flag=wx.ALIGN_CENTER | wx.TOP, border=15)
+        panel.SetSizer(vsizer_all)
+
+    def OnclickSubmit(self, event):
+        """ 点击确定按钮，执行方法 """
+        message = ""
+        username = self.text_user.GetValue()  # 获取输入的用户名
+        password = self.text_password.GetValue()  # 获取输入的密码
+        if username == "" or password == "":  # 判断用户名或密码是否为空
+            message = '用户名或密码不能为空'
+        elif username == 'admin' and password == '123456':  # 用户名和密码正确
+            message = '登录成功'
+        else:
+            message = '用户名和密码不匹配'  # 用户名或密码错误
+        wx.MessageBox(message)  # 弹出提示框
+
+    def OnclickCancel(self, event):  # 没有event点击取消会报错
+        """ 点击取消按钮，执行方法 """
+        self.text_user.SetValue("")  # 清空输入的用户名
+        self.text_password.SetValue("")  # 清空输入的密码
+
+
+if __name__ == '__main__':
+    app = wx.App()  # 初始化
+    frame = MyFrame(parent=None, id=-1)  # 实例MyFrame类，并传递参数
+    frame.Show()  # 显示窗口
+    app.MainLoop()  # 调用主循环方法
