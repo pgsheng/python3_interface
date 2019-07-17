5ad7a77c1bc52f0f2bc6ffa0e4dd5cdd959f3c98
pgsheng <pgsheng09@163.com>
2019-07-17 15:40:06 +0800

    优化

--- study_case/gupiao.py
+++ study_case/gupiao.py
@@ -30,22 +30,7 @@ class MyFrame(wx.Frame):
             horizontal_user = wx.BoxSizer(wx.HORIZONTAL)  # 容器中控件按横向并排排列
             for st in st_list:
                 box = wx.StaticText(panel, label=str(st))
-            # name_st = wx.StaticText(panel, label=name)
-            # zuotian_st = wx.StaticText(panel, label=zuotian)
-            # kaipai_st = wx.StaticText(panel, label=kaipai)
-            # dangqian_st = wx.StaticText(panel, label=dangqian)
-            # zuigao_st = wx.StaticText(panel, label=zuigao)
-            # zuidi_st = wx.StaticText(panel, label=zuidi)
-            # jine_st = wx.StaticText(panel, label=str(jine))
-            # baifenbi = wx.StaticText(panel, label=str(baifenbi))
                 horizontal_user.Add(box, proportion=1, flag=wx.ALIGN_CENTER_VERTICAL, border=5)
-                # horizontal_user.Add(zuotian_st)
-                # horizontal_user.Add(kaipai_st)
-                # horizontal_user.Add(dangqian_st)
-                # horizontal_user.Add(zuigao_st)
-                # horizontal_user.Add(zuidi_st)
-                # horizontal_user.Add(jine_st)
-                # horizontal_user.Add(baifenbi)
             vsizer_all.Add(horizontal_user)
         panel.SetSizer(vsizer_all)
         # Timer(20, self.show_wain).start()
@@ -59,3 +44,30 @@ if __name__ == '__main__':
     frame = MyFrame(parent=None, id=-1)  # 实例MyFrame类，并传递参数
     frame.Show()  # 显示窗口
     app.MainLoop()  # 调用主循环方法
+
+
+# 0：”大秦铁路”，股票名字；
+# 1：”27.55″，今日开盘价；
+# 2：”27.25″，昨日收盘价；
+# 3：”26.91″，当前价格；
+# 4：”27.55″，今日最高价；
+# 5：”26.20″，今日最低价；
+# 6：”26.91″，竞买价，即“买一”报价；
+# 7：”26.92″，竞卖价，即“卖一”报价；
+# 8：”22114263″，成交的股票数，由于股票交易以一百股为基本单位，所以在使用时，通常把该值除以一百；
+# 9：”589824680″，成交金额，单位为“元”，为了一目了然，通常以“万元”为成交金额的单位，所以通常把该值除以一万；
+# 10：”4695″，“买一”申请4695股，即47手；
+# 11：”26.91″，“买一”报价；
+# 12：”57590″，“买二”
+# 13：”26.90″，“买二”
+# 14：”14700″，“买三”
+# 15：”26.89″，“买三”
+# 16：”14300″，“买四”
+# 17：”26.88″，“买四”
+# 18：”15100″，“买五”
+# 19：”26.87″，“买五”
+# 20：”3100″，“卖一”申报3100股，即31手；
+# 21：”26.92″，“卖一”报价
+# (22, 23), (24, 25), (26,27), (28, 29)分别为“卖二”至“卖四的情况”
+# 30：”2008-01-11″，日期；
+# 31：”15:05:32″，时间；
